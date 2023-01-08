84a59a96e50e6158b56cf92da8ca124097bc16c2
Akhilesh Singh <103947154+akhileshsingh9690@users.noreply.github.com>
2023-01-08 20:18:05 +0530

    Update Sample.jsx

--- my-react-learnings/src/Components/Sample.jsx
+++ my-react-learnings/src/Components/Sample.jsx
@@ -2,27 +2,33 @@ import React from "react";
 import { useEffect } from "react";
 const Sample=()=>{
 
+
+const handlepageji = (cat)=>{
+setPage(page + cat)
+}
+
+
 const [data,setData] = React.useState([]);
 const [loading,setLoading]=React.useState(false);
 const [page,setPage]= React.useState(1)
 useEffect(()=>{
  
-    addhandlefetch()
+    addhandlefetch(page)
 
-},[])
+},[page])
 
 
 
-const getData=()=>{
-    return fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10`).then((res)=>res.json());
+const getData=(page)=>{
+    return fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`).then((res)=>res.json());
 }
 
 
 
-const addhandlefetch= async()=>{
+const addhandlefetch= async(page)=>{
 setLoading(true)
 try {
-    let datass = await getData()
+    let datass = await getData(page)
     setData(datass)
     setLoading(false)
     console.log(datass)    
@@ -47,9 +53,9 @@ try {
  {data.map((el)=>(<li>{el.title}</li>))}
 
    </ul>
- <button onClick={()=>{setPage(page )}}>Previous</button>
- <button>{page}</button>   
- <button>Next</button>   
+ <button onClick={()=>handlepageji(-1)} disabled={page === 1}>Previous</button>
+ <button disabled>{page}</button>   
+ <button disabled={page >= 10} onClick={()=>handlepageji(1)}>Next</button>   
 
    </div>
  
