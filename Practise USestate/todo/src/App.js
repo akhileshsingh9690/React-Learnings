ab10e4d7ad810759b7a030e29637826da8cdc2c2
Akhilesh Singh <103947154+akhileshsingh9690@users.noreply.github.com>
2023-01-19 19:54:12 +0530

    Update App.js

--- Practise USestate/todo/src/App.js
+++ Practise USestate/todo/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const [text , setText]=React.useState("")
+
+ 
+const handleInputText=(e)=>{
+  setText(e.target.value)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+
+   <>
+   <h4>Text : {text}</h4>
+   <input value={text} placeholder="Add Todo here"
+   onChange={handleInputText}
+   />
+</>
     </div>
   );
 }
